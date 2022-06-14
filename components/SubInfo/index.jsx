ba8eb5e79097f0d06315d4312081e5cd349ca051
mubshergafar <mubsher.gafar@outlook.com>
2022-06-14 16:55:22 +0400

    first commit

---
+++ components/SubInfo/index.jsx
@@ -0,0 +1,10 @@
+import {
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+  ImageCmp,
+  People,
+  EndDate,
+} from "./SubInfo";
+
+export { SubInfo, EthPrice, NFTTitle, ImageCmp, People, EndDate };
---
+++ components/index.jsx
@@ -0,0 +1,17 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Buttons/Button";
+import { SubInfo } from "./SubInfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+export {
+  FocusedStatusBar,
+  NFTCard,
+  HomeHeader,
+  SubInfo,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+};
